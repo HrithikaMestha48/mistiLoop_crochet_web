b66dabbb670c5e1caa335c00d39adb5880f5bdcf
HrithikaMestha <hrithikamestha48@gmail.com>
2026-05-31 00:17:33 +0530

    Added the Home Page

---
+++ mistiloopWeb/mistiloopWeb/src/main/java/com/mistiloop/mistiloopWeb/MistiloopWebApplication.java
@@ -0,0 +1,13 @@
+package com.mistiloop.mistiloopWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MistiloopWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MistiloopWebApplication.class, args);
+	}
+
+}
